22edf6f4a482606fb47de97e3c997d824ecde245
weeger <romain.weeger@gmail.com>
2016-11-13 18:56:28 +0100

    First search event

--- src/script.js
+++ src/script.js
@@ -7,12 +7,54 @@
 
   var GVMap = class {
     constructor() {
+      // Load the first non semantic database.
       this.ajax({
         url: '../src/data.json', success: (e) => {
-          log(JSON.parse(e.responseText));
-          this.data = {};
+          this.data = JSON.parse(e.responseText);
+          log(this.data);
+          // Shortcuts.
+          this.searchTextInput = this.domId('searchText');
+          // Listeners.
+          this.listen('searchForm', 'submit', (e) => {
+            e.preventDefault();
+            this.search(this.searchTextInput.value);
+          });
+          this.searchTextInput.focus();
+          // TODO temp
+          this.search('des');
         }
       });
+    }
+
+    search(term) {
+      let results = [];
+      let value;
+      // This search method is temporary.
+      // Iterates over items.
+      for (let itemId in this.data) {
+        // Iterates over fields.
+        for (let key in this.data[itemId]) {
+          value = this.data[itemId][key];
+          if (value && value.indexOf && value.indexOf(term) !== -1) {
+            // Use id as key to prevent duplicates.
+            results[itemId] = itemId;
+          }
+        }
+      }
+
+      log(results);
+    }
+
+    listen(id, event, callback) {
+      return this.domId(id).addEventListener(event, callback);
+    }
+
+    domId(id) {
+      return document.getElementById(id);
+    }
+
+    dom(selector) {
+      return document.querySelectorAll(selector);
     }
 
     /**
